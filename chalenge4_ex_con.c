047cb7b45c46da9c4d2f6d4f53471785aa50d6b4
ilyasns <ilyasns05@outlook.com>
2025-09-23 18:54:09 +0100

    fonction_2_degree

--- chalenge4_ex_con.c
+++ chalenge4_ex_con.c
@@ -1,1 +1,33 @@
-#
+#include <stdio.h>
+
+
+
+int ft_sum_triangle(int x, int y, int z)
+{
+  int sum = x + y;
+  if (x + y > z && x + z > y && y + z > x)
+  {
+    if (x == y && y == z)
+    {
+      printf("Équilatéral");
+    }
+    else if (x == y || x == z || y == z)
+    {
+      printf("Isocèle");
+    }
+    else
+    {
+      printf ("Scalène");
+    }
+  }
+  else
+  printf("error");
+}
+int main()
+{
+  int a = 4;
+  int b = 9;
+  int c = 4;
+  ft_sum_triangle(a, b, c);
+  
+}
